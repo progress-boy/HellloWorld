e6b2e15b40fe2d8357f4657b7f6ecc5976836568
progress-boy <448630396@qq.com>
2017-02-07 14:58:49 +0800

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import sys
+import re
+
+def main():
+    print "hello world"
+
+if ___name__ == '__main__':
+    #带参数
+    input = sys.argv[1]
+    ouput = sys.argv[2]
+    main()  #函数入口
